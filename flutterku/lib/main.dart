ce95f6811675273a1fb7cea2f629bf9c83460d72
ikhsan15 <ikhsan15nur@gmail.com>
2022-01-27 17:26:09 +0700

    TextStyle

--- flutterku/lib/main.dart
+++ flutterku/lib/main.dart
@@ -4,41 +4,30 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String message = "ini adalah text";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Anonymous Method"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(message),
-              ElevatedButton(
-                  onPressed: () {
-                    // ini adalah contoh anonymous method, method yg hanya di panggil di sini saja
-                    setState(() {
-                      message = "Tombol sudah ditekan";
-                    });
-                  },
-                  child: Text("tekan saya"))
-            ],
+          appBar: AppBar(
+            title: const Text("latihan text style"),
           ),
-        ),
-      ),
+          body: const Center(
+              child: Text(
+            "ini adalah text",
+            style: TextStyle(
+              fontFamily: "SansPro",
+              fontStyle: FontStyle.italic,
+              fontSize: 30,
+              decoration: TextDecoration.overline,
+              decorationColor: Colors.red,
+              decorationThickness: 5,
+              decorationStyle: TextDecorationStyle.dotted,
+            ),
+          ))),
     );
   }
 }
