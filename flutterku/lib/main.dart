511f3d4c1645e3351a762178de834e5dbe4597e9
ikhsan15 <ikhsan15nur@gmail.com>
2022-02-07 15:01:33 +0700

    listview

--- flutterku/lib/main.dart
+++ flutterku/lib/main.dart
@@ -1,33 +1,72 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // new widget
+  List<Widget> widgets = [];
+  int counter = 1;
+
+  // // new constructor
+  // _MyAppState() {
+  //   for (int i = 0; i < 20; i++) {
+  //     widgets.add(Text(
+  //       "data ke " + i.toString(),
+  //       style: const TextStyle(fontSize: 40),
+  //     ));
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text("latihan text style"),
+        appBar: AppBar(
+          title: const Text("latihan listview"),
+        ),
+        body: ListView(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      widgets.add(Text(
+                        "data ke " + counter.toString(),
+                        style: const TextStyle(fontSize: 40),
+                      ));
+                      counter++;
+                    });
+                  },
+                  child: const Text("tambah data")),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      widgets.removeLast();
+                      counter--;
+                    });
+                  },
+                  child: const Text("hapus data")),
+            ],
           ),
-          body: const Center(
-              child: Text(
-            "ini adalah text",
-            style: TextStyle(
-              fontFamily: "SansPro",
-              fontStyle: FontStyle.italic,
-              fontSize: 30,
-              decoration: TextDecoration.overline,
-              decorationColor: Colors.red,
-              decorationThickness: 5,
-              decorationStyle: TextDecorationStyle.dotted,
-            ),
-          ))),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: widgets,
+          )
+        ]),
+      ),
     );
   }
 }
